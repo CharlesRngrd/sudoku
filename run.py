0a315877b0b7b0ebf19719a9b1d13d30752dc83e
charles <charles.rangheard@hotmail.fr>
2025-10-23 15:29:36 +0200

    Ajout du niveau 4

--- run.py
+++ run.py
@@ -1,54 +1,74 @@
 from sudoku import Game
 
-sudoku_demo = [
-    [5, 3, None, None, 7, None, None, None, None],
-    [6, None, None, 1, 9, 5, None, None, None],
-    [None, 9, 8, None, None, None, None, 6, None],
-    [8, None, None, None, 6, None, None, None, 3],
-    [4, None, None, 8, None, 3, None, None, 1],
-    [7, None, None, None, 2, None, None, None, 6],
-    [None, 6, None, None, None, None, 2, 8, None],
-    [None, None, None, 4, 1, 9, None, None, 5],
-    [None, None, None, None, 8, None, None, 7, 9],
+sudokus = [
+    [
+        [None, 2, None, None, 4, None, 8, 1, None],
+        [None, None, None, 6, 2, None, 3, None, 4],
+        [None, None, None, 1, 9, 8, None, None, 5],
+        [None, None, None, None, None, 4, 1, 2, None],
+        [7, None, None, 2, None, None, 5, None, 9],
+        [None, None, 9, None, None, 5, None, None, None],
+        [None, 6, 8, None, None, None, None, 7, None],
+        [3, 4, 2, None, 8, None, None, None, None],
+        [None, None, None, None, 5, None, None, None, 8],
+    ],
+    [
+        [7, 4, 5, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, 9, None],
+        [None, 9, 6, None, 8, 4, 7, None, None],
+        [None, 6, 1, None, None, 8, None, None, 4],
+        [None, None, 9, 7, None, 3, 1, 8, None],
+        [3, None, None, None, 1, None, None, None, 2],
+        [None, 8, None, None, None, None, None, None, None],
+        [None, None, None, None, 3, 1, None, None, 9],
+        [None, None, None, None, None, None, 8, 2, 7],
+    ],
+    [
+        [None, None, None, None, 3, 2, None, 6, 8],
+        [None, None, None, 5, None, 8, None, None, 3],
+        [None, None, None, 7, None, 4, None, None, 1],
+        [4, 3, 5, None, 7, None, None, None, 2],
+        [None, None, 6, None, None, 9, None, 8, None],
+        [None, None, None, None, None, None, 3, None, None],
+        [1, 9, None, None, None, None, None, 5, None],
+        [None, None, None, 9, None, None, None, None, None],
+        [5, None, 4, None, 8, None, 7, None, 9],
+    ],
+    [
+        [None, None, None, 9, None, 2, None, 8, 7],
+        [7, 6, None, None, 8, None, None, None, None],
+        [None, 2, None, 3, None, None, 5, None, None],
+        [None, None, None, None, None, None, 8, None, 3],
+        [2, None, None, 7, 5, None, 1, None, None],
+        [None, None, None, 2, None, None, None, None, 4],
+        [None, None, None, 5, None, None, 9, 4, None],
+        [5, None, None, None, None, None, None, 3, None],
+        [8, None, 1, 6, 4, 9, None, None, None],
+    ],
+    [
+        [None, None, 5, 6, None, 2, None, None, None],
+        [7, None, None, 8, 1, None, 4, None, None],
+        [None, None, None, None, None, None, 5, 6, None],
+        [None, 4, 9, None, None, None, None, None, None],
+        [8, None, None, None, None, None, None, None, 7],
+        [None, None, None, None, 5, 1, None, None, None],
+        [None, None, None, 9, 4, None, 8, None, 2],
+        [3, None, 6, 1, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+    ],
+    [
+        [None, None, 5, None, 7, 2, None, None, None],
+        [4, 3, 8, None, None, None, None, None, None],
+        [None, None, 7, None, 1, None, None, None, None],
+        [None, None, None, None, None, 4, None, None, None],
+        [None, 5, None, None, None, 8, None, 3, None],
+        [None, None, None, 7, None, None, 4, 8, None],
+        [3, None, None, None, None, None, None, None, 5],
+        [None, 6, None, None, None, None, 7, None, None],
+        [None, None, None, 6, 9, None, None, None, 2],
+    ],
 ]
 
-sudoku_level1 = [
-    [None, None, None, None, None, None, 7, None, 6],
-    [None, 7, None, 9, None, 6, 3, None, None],
-    [None, 5, None, 4, 7, 3, 8, 9, None],
-    [1, 3, 2, None, None, 5, None, 7, 9],
-    [None, None, 4, 2, 1, None, 5, 3, 8],
-    [None, 9, None, None, None, 4, None, None, None],
-    [None, 2, 7, None, 6, 8, None, 1, 3],
-    [None, None, None, None, 4, None, None, 8, None],
-    [6, None, None, None, 9, 7, None, None, 4],
-]
-
-sudoku_level2 = [
-    [8, 9, None, None, 7, 2, 5, None, 1],
-    [5, None, 7, None, 3, None, 6, None, None],
-    [None, 6, 3, 5, None, 9, None, 2, 7],
-    [3, None, None, None, None, 7, None, None, None],
-    [9, None, None, None, 8, None, 7, None, None],
-    [None, None, 5, None, None, None, 2, None, None],
-    [None, None, None, 4, 1, 5, None, 6, None],
-    [None, 4, None, None, 6, None, None, None, None],
-    [None, None, None, None, None, None, 3, None, None],
-]
-
-sudoku_level3 = [
-    [7, 4, 5, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, 9, None],
-    [None, 9, 6, None, 8, 4, 7, None, None],
-    [None, 6, 1, None, None, 8, None, None, 4],
-    [None, None, 9, 7, None, 3, 1, 8, None],
-    [3, None, None, None, 1, None, None, None, 2],
-    [None, 8, None, None, None, None, None, None, None],
-    [None, None, None, None, 3, 1, None, None, 9],
-    [None, None, None, None, None, None, 8, 2, 7],
-]
-
-
-for sudoku in [sudoku_demo, sudoku_level1, sudoku_level2, sudoku_level3]:
+for sudoku in sudokus:
     game = Game(sudoku)
     game.process()
--- sudoku.py
+++ sudoku.py
@@ -38,11 +38,21 @@ class Game:
 
                 while not self.STOP_ITERATION:
                     self.disable_values(number)
-                    self.check_lines(number)
-                    self.check_columns(number)
+
+                    self.STOP_ITERATION = not any(
+                        [
+                            self.check_lines(number),
+                            self.check_columns(number),
+                            self.check_blocs(number),
+                        ]
+                    )
 
                     if not self.STOP_ITERATION:
                         self.STOP_PROCESS = False
+
+            self.disable_values(number)
+            if self.check_unique_possibility():
+                self.STOP_ITERATION = False
 
         self.assert_finish()
 
@@ -66,22 +76,7 @@ class Game:
                 if any([same_line, same_col, same_block]):
                     self.available[number][i_line][i_cell] = None
 
-    def disable_block(self, number, line_values):
-        if len(line_values) == 2 and line_values[0][1] // 3 == line_values[1][1] // 3:
-            for i_line, i_cell, _, cell in self.get_cells(self.available[number]):
-                if (
-                    cell == number + 1
-                    and i_line // 3 == line_values[0][0] // 3
-                    and i_cell // 3 == line_values[0][1] // 3
-                ):
-                    self.available[number][i_line][i_cell] = None
-
-            self.available[number][line_values[0][0]][line_values[0][1]] = number + 1
-            self.available[number][line_values[1][0]][line_values[1][1]] = number + 1
-
     def check_lines(self, number):
-        self.STOP_ITERATION = True
-
         for i_line, line in enumerate(self.available[number]):
             line_values = [
                 [i_line, i_cell]
@@ -92,13 +87,9 @@ class Game:
             if len(line_values) == 1:
                 self.sudoku[line_values[0][0]][line_values[0][1]] = number + 1
 
-                self.STOP_ITERATION = False
-
-            self.disable_block(number, line_values)
+                return True
 
     def check_columns(self, number):
-        self.STOP_ITERATION = True
-
         for column in range(9):
             column_values = [
                 [i_line, column]
@@ -109,7 +100,68 @@ class Game:
             if len(column_values) == 1:
                 self.sudoku[column_values[0][0]][column_values[0][1]] = number + 1
 
-                self.STOP_ITERATION = False
+                return True
+
+    def check_blocs(self, number):
+        for line_bloc in range(3):
+            for column_bloc in range(3):
+                block_values = []
+
+                for i_line, i_cell, _, cell in self.get_cells(self.available[number]):
+                    if (
+                        cell == number + 1
+                        and i_line // 3 == line_bloc
+                        and i_cell // 3 == column_bloc
+                    ):
+                        block_values.append([i_line, i_cell])
+
+                if len(block_values) > 1:
+                    if len(set([value[0] for value in block_values])) == 1:
+                        for i_line, i_cell, _, _ in self.get_cells(
+                            self.available[number]
+                        ):
+                            if i_line == block_values[0][0]:
+                                if (
+                                    self.available[number][i_line][i_cell] is not None
+                                    and i_cell // 3 != column_bloc
+                                ):
+                                    self.available[number][i_line][i_cell] = None
+
+                                    return True
+
+                    if len(set([value[1] for value in block_values])) == 1:
+                        for i_line, i_cell, _, _ in self.get_cells(
+                            self.available[number]
+                        ):
+                            if i_cell == block_values[0][1]:
+                                if (
+                                    self.available[number][i_line][i_cell] is not None
+                                    and i_line // 3 != line_bloc
+                                ):
+                                    self.available[number][i_line][i_cell] = None
+
+                                    return True
+
+                if len(block_values) == 1:
+                    self.sudoku[block_values[0][0]][block_values[0][1]] = number + 1
+
+                    return True
+
+    def check_unique_possibility(self):
+        possibility = [[[] for _ in range(9)] for _ in range(9)]
+
+        for number in range(9):
+            for i_line, i_cell, _, cell in self.get_cells(self.available[number]):
+                if cell is None:
+                    continue
+
+                possibility[i_line][i_cell].append(cell)
+
+        for i_line, i_cell, _, cell in self.get_cells(possibility):
+            if len(set(cell)) == 1:
+                if self.sudoku[i_line][i_cell] is None:
+                    self.sudoku[i_line][i_cell] = cell[0]
+                    return True
 
     def assert_finish(self):
         has_wrong_line = any([len(set(line)) != 9 for line in self.sudoku])
@@ -122,7 +174,6 @@ class Game:
         )
 
         if has_wrong_line or has_wrong_column:
-            self.print_grid(self.sudoku)
-            raise AssertionError("Sodoku resolution failed !")
+            print("Sodoku resolution failed !")
 
         self.print_grid(self.sudoku)
